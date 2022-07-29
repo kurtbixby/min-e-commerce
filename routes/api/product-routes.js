f4694c66d02fe92ef4155b89cbe319cbbaffc54f
Kurt Bixby <kurtbixby@mac.com>
2022-07-29 10:09:05 -0500

    Indentation adjustment

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -95,27 +95,27 @@ router.put('/:id', async (req, res) => {
     const productTags = ProductTag.findAll({ where: { product_id: req.params.id } });
 
     const productTagIds = productTags.map(({ tag_id }) => tag_id);
-      // create filtered list of new tag_ids
-      const newProductTags = req.body.tagIds
-        .filter((tag_id) => !productTagIds.includes(tag_id))
-        .map((tag_id) => {
-          return {
-            product_id: req.params.id,
-            tag_id,
-          };
-        });
-      // figure out which ones to remove
-      const productTagsToRemove = productTags
-        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-        .map(({ id }) => id);
+    // create filtered list of new tag_ids
+    const newProductTags = req.body.tagIds
+      .filter((tag_id) => !productTagIds.includes(tag_id))
+      .map((tag_id) => {
+        return {
+          product_id: req.params.id,
+          tag_id,
+        };
+      });
+    // figure out which ones to remove
+    const productTagsToRemove = productTags
+      .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+      .map(({ id }) => id);
 
-      // run both actions
-      const updatedProductTags = await Promise.all([
-        ProductTag.destroy({ where: { id: productTagsToRemove } }),
-        ProductTag.bulkCreate(newProductTags),
-      ]);
+    // run both actions
+    const updatedProductTags = await Promise.all([
+      ProductTag.destroy({ where: { id: productTagsToRemove } }),
+      ProductTag.bulkCreate(newProductTags),
+    ]);
 
-      res.json(updatedProductTags)
+    res.json(updatedProductTags)
   } catch (err) {
     // console.log(err);
     res.status(400).json(err);
